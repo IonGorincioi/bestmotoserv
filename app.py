357fc03209f5253229e58cef118d83bfb9c790f0
Ion Gorincioi <49958966+IonGorincioi@users.noreply.github.com>
2022-07-05 12:41:48 +0100

    Created routes & linked with simple templates

--- app.py
+++ app.py
@@ -1,6 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contacts')
+def contacts():
+    return render_template('contacts.html')
+
+@app.route('/pricing')
+def pricing():
+    return render_template('pricing.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
 if __name__=="__main__":
     app.run(debug=True)
